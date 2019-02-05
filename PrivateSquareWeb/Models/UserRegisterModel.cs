700fc9e12b36176443093ac3fd40a59a4d037185
nehaelite <nehacityinfomart@gmail.com>
2019-02-05 18:52:52 +0530

    Add partial View OTP.cshtml

--- PrivateSquareWeb/Models/UserRegisterModel.cs
+++ PrivateSquareWeb/Models/UserRegisterModel.cs
@@ -10,13 +10,13 @@ namespace PrivateSquareWeb.Models
     {
         public long Id { get; set; }
         [Required(ErrorMessage = "Mobile number is required.")]
-        [MaxLength(12,ErrorMessage ="Please Enter Less Then 12 Character")]
-        [MinLength(10,ErrorMessage ="Please Enter Onlt 10 Digit")]
+        // [MaxLength(12,ErrorMessage ="Please Enter Less Then 12 Character")]
+        [MinLength(10,ErrorMessage ="Enter 10 Digit")]
         [RegularExpression("^[0-9]*$", ErrorMessage = "Mobile must be numeric")]
         public string Mobile { get; set; }
         [Required(ErrorMessage = "OTP is required.")]
-        [MaxLength(6, ErrorMessage = "Please Enter Less Then 6 Character")]
-        [MinLength(4, ErrorMessage = "Please Enter Onlt 4 Digit")]
+       // [MaxLength(6, ErrorMessage = "Please Enter Less Then 6 Character")]
+        [MinLength(4, ErrorMessage = "Enter 4 Digit OTP Number.")]
         [RegularExpression("^[0-9]*$", ErrorMessage = "OTP must be numeric")]
         public string Otp { get; set; }
     }
