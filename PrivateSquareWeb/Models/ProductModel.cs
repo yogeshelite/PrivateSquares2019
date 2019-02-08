a32df082a330446b66b859ba7a2034a3f14a5632
sandeepelite <sandeepsharma@accendos.in>
2019-02-08 19:04:59 +0530

    Added validation to Add product and Add business

--- PrivateSquareWeb/Models/ProductModel.cs
+++ PrivateSquareWeb/Models/ProductModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,11 @@ namespace PrivateSquareWeb.Models
     public class ProductModel
     {
         public long Id { get; set; }
+        [Required(ErrorMessage = "Product Name cannot be empty")]
         public string ProductName { get; set; }
         public long ProductCatId { get; set; }
         public string ProductImage { get; set; }
+        [Required(ErrorMessage ="Selling Price cannot be empty")]
         public decimal SellingPrice { get; set; }
         public decimal DiscountPrice { get; set; }
         public long BusinessId { get; set; }
