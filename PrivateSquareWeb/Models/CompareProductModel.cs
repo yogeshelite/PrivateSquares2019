c05885c528ba7310dafe5c1aeec3fc89750a816d
nehaelite <nehacityinfomart@gmail.com>
2019-03-14 19:01:17 +0530

    Web Compare Products

---
+++ PrivateSquareWeb/Models/CompareProductModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PrivateSquareWeb.Models
+{
+    public class CompareProductModel
+    {
+        public long Id { get; set; }
+        [Required(ErrorMessage = "Product Name cannot be empty")]
+        public string ProductName { get; set; }
+        public long ProductCatId { get; set; }
+        public string ProductImage { get; set; }
+        [Required(ErrorMessage = "Selling Price cannot be empty")]
+        public decimal SellingPrice { get; set; }
+        public decimal DiscountPrice { get; set; }
+        public long BusinessId { get; set; }
+        public long? UserId { get; set; }
+        public string Description { get; set; }
+        public string XmlProductImage { get; set; }
+        public string ProductImages { get; set; }
+        public string VendorName { get; set; }
+        public string Operation { get; set; }
+    }
+}
