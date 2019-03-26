012bdb52e17d01573c6f090e478f771a2b46d12a
nehaelite <nehacityinfomart@gmail.com>
2019-03-26 16:08:20 +0530

    Added WebSidebar,Sign Up validation

---
+++ PrivateSquareWeb/Controllers/Website/WebSidebarController.cs
@@ -0,0 +1,29 @@
+﻿using PrivateSquareWeb.CommonCls;
+using PrivateSquareWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PrivateSquareWeb.Controllers.Website
+{
+    public class WebSidebarController : Controller
+
+    {
+       
+        // GET: WebSidebar
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult WebSiteSidebar()
+        {
+            ProductModel objModel = new ProductModel();
+            var ProductCatList = CommonFile.GetProductCategory();
+            ViewBag.ProductCatList = ProductCatList;
+     
+            return PartialView("~/Views/Shared/_WebSiteSidebar.cshtml", objModel);
+        }
+    }
+}
